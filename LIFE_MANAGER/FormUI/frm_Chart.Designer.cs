6f52818a878a6a3247554a6573a0bb9cdff7b21d
Ceasar Tran <tranquocc1@gmail.com>
2022-06-09 15:53:07 +0700

    Added pie chart

--- LIFE_MANAGER/FormUI/frm_Chart.Designer.cs
+++ LIFE_MANAGER/FormUI/frm_Chart.Designer.cs
@@ -30,8 +30,19 @@ namespace LIFE_MANAGER.FormUI
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Chart));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.btn_Close = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.dtpk_Time = new System.Windows.Forms.DateTimePicker();
+            this.lb_MoodChart = new System.Windows.Forms.Label();
+            this.PieChart_Mood = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lb_HappyDays = new System.Windows.Forms.Label();
+            this.lb_NeutralDays = new System.Windows.Forms.Label();
+            this.lb_SadDays = new System.Windows.Forms.Label();
+            this.lb_NotSet = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.PieChart_Mood)).BeginInit();
             this.SuspendLayout();
             // 
             // btn_Close
@@ -56,18 +67,104 @@ namespace LIFE_MANAGER.FormUI
             this.label1.TabIndex = 7;
             this.label1.Text = "Chart";
             // 
+            // dtpk_Time
+            // 
+            this.dtpk_Time.CustomFormat = "MM / yyyy";
+            this.dtpk_Time.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpk_Time.Location = new System.Drawing.Point(406, 64);
+            this.dtpk_Time.Name = "dtpk_Time";
+            this.dtpk_Time.ShowUpDown = true;
+            this.dtpk_Time.Size = new System.Drawing.Size(260, 22);
+            this.dtpk_Time.TabIndex = 8;
+            this.dtpk_Time.ValueChanged += new System.EventHandler(this.dtpk_Time_ValueChanged);
+            // 
+            // lb_MoodChart
+            // 
+            this.lb_MoodChart.AutoSize = true;
+            this.lb_MoodChart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_MoodChart.Location = new System.Drawing.Point(108, 92);
+            this.lb_MoodChart.Name = "lb_MoodChart";
+            this.lb_MoodChart.Size = new System.Drawing.Size(165, 31);
+            this.lb_MoodChart.TabIndex = 9;
+            this.lb_MoodChart.Text = "Mood Chart";
+            // 
+            // PieChart_Mood
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.PieChart_Mood.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.PieChart_Mood.Legends.Add(legend1);
+            this.PieChart_Mood.Location = new System.Drawing.Point(41, 126);
+            this.PieChart_Mood.Name = "PieChart_Mood";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.PieChart_Mood.Series.Add(series1);
+            this.PieChart_Mood.Size = new System.Drawing.Size(400, 300);
+            this.PieChart_Mood.TabIndex = 10;
+            this.PieChart_Mood.Text = "chart1";
+            // 
+            // lb_HappyDays
+            // 
+            this.lb_HappyDays.AutoSize = true;
+            this.lb_HappyDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_HappyDays.Location = new System.Drawing.Point(447, 126);
+            this.lb_HappyDays.Name = "lb_HappyDays";
+            this.lb_HappyDays.Size = new System.Drawing.Size(87, 29);
+            this.lb_HappyDays.TabIndex = 11;
+            this.lb_HappyDays.Text = "Happy";
+            // 
+            // lb_NeutralDays
+            // 
+            this.lb_NeutralDays.AutoSize = true;
+            this.lb_NeutralDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_NeutralDays.Location = new System.Drawing.Point(447, 157);
+            this.lb_NeutralDays.Name = "lb_NeutralDays";
+            this.lb_NeutralDays.Size = new System.Drawing.Size(98, 29);
+            this.lb_NeutralDays.TabIndex = 12;
+            this.lb_NeutralDays.Text = "Neutral";
+            // 
+            // lb_SadDays
+            // 
+            this.lb_SadDays.AutoSize = true;
+            this.lb_SadDays.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_SadDays.Location = new System.Drawing.Point(447, 188);
+            this.lb_SadDays.Name = "lb_SadDays";
+            this.lb_SadDays.Size = new System.Drawing.Size(59, 29);
+            this.lb_SadDays.TabIndex = 13;
+            this.lb_SadDays.Text = "Sad";
+            // 
+            // lb_NotSet
+            // 
+            this.lb_NotSet.AutoSize = true;
+            this.lb_NotSet.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_NotSet.Location = new System.Drawing.Point(447, 219);
+            this.lb_NotSet.Name = "lb_NotSet";
+            this.lb_NotSet.Size = new System.Drawing.Size(100, 29);
+            this.lb_NotSet.TabIndex = 14;
+            this.lb_NotSet.Text = "Not Set";
+            // 
             // frm_Chart
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1062, 468);
+            this.Controls.Add(this.lb_NotSet);
+            this.Controls.Add(this.lb_SadDays);
+            this.Controls.Add(this.lb_NeutralDays);
+            this.Controls.Add(this.lb_HappyDays);
+            this.Controls.Add(this.PieChart_Mood);
+            this.Controls.Add(this.lb_MoodChart);
+            this.Controls.Add(this.dtpk_Time);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Close);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frm_Chart";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Chart";
+            ((System.ComponentModel.ISupportInitialize)(this.PieChart_Mood)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,5 +174,12 @@ namespace LIFE_MANAGER.FormUI
 
         private System.Windows.Forms.Button btn_Close;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpk_Time;
+        private System.Windows.Forms.Label lb_MoodChart;
+        private System.Windows.Forms.DataVisualization.Charting.Chart PieChart_Mood;
+        private System.Windows.Forms.Label lb_HappyDays;
+        private System.Windows.Forms.Label lb_NeutralDays;
+        private System.Windows.Forms.Label lb_SadDays;
+        private System.Windows.Forms.Label lb_NotSet;
     }
 }
--- LIFE_MANAGER/FormUI/frm_Chart.cs
+++ LIFE_MANAGER/FormUI/frm_Chart.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,15 +13,105 @@ namespace LIFE_MANAGER.FormUI
 {
     public partial class frm_Chart : Form
     {
+        private IMongoCollection<Models.Diary> Diaries = frm_Login.db.GetCollection<Models.Diary>("Diaries");
+        private IMongoCollection<Models.Plan> Plans = frm_Login.db.GetCollection<Models.Plan>("Plans");
+        private int HappyDays = 0;
+        private int NeutralDays = 0;
+        private int SadDays = 0;
+        private int NotSet = 0;
+        private DateTime Today = DateTime.Now;
         public frm_Chart()
         {
             InitializeComponent();
+            dtpk_Time.Value = Today;
+            GetMoodChartValue(Today);
+            PieChart_Mood.Titles.Add("Mood Chart");
+            PieChart_Mood.Series["Series1"].Points.AddXY("Happy", HappyDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Neutral", NeutralDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Sad", SadDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Not Set", NotSet);
+            setStaticsLabel();
         }
 
         private void btn_Close_Click(object sender, EventArgs e)
         {
             this.Close();
 
+        }
+
+        private void dtpk_Time_ValueChanged(object sender, EventArgs e)
+        {
+            GetMoodChartValue(dtpk_Time.Value);
+            PieChart_Mood.Series["Series1"].Points.Clear();
+            PieChart_Mood.Series["Series1"].Points.AddXY("Happy", HappyDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Neutral", NeutralDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Sad", SadDays);
+            PieChart_Mood.Series["Series1"].Points.AddXY("Not Set", NotSet);
+            setStaticsLabel();
+        }
+        private void GetMoodChartValue(DateTime date)
+        {
+            HappyDays = 0;
+            NeutralDays = 0;
+            NotSet = 0;
+            SadDays = 0;
+            var DateMDY = date.ToString("M/d/yyyy");
+            var DateSplited = DateMDY.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                var MoodQuery = Diaries.Find(dateD => dateD.DateSplit[0] == DateSplited[2] && dateD.DateSplit[1] == DateSplited[0] && dateD.UserId == frm_Login.User._id).ToList();
+                for (int i = 0; i < MoodQuery.Count; i++)
+                {
+                    switch(MoodQuery[i].Mood)
+                    {
+                        case "Happy":
+                            HappyDays++;
+                            break;
+                        case "Neutral":
+                            NeutralDays++;
+                            break;
+                        case "Sad":
+                            SadDays++;
+                            break;
+                        default:
+                            NotSet++;
+                            break;
+                    }
+                }
+                NotSet = DayOfMonth(Today) - MoodQuery.Count;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        int DayOfMonth(DateTime date)
+        {
+            switch (date.Month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    return 31;
+                case 2:
+                    if ((date.Year % 4 == 0 && date.Year % 100 != 0) || date.Year % 400 == 0)
+                        return 29;
+                    else
+                        return 28;
+                default:
+                    return 30; ;
+            }
+        }
+        private void setStaticsLabel()
+        {
+            lb_HappyDays.Text = "Happy days: " + HappyDays;
+            lb_NeutralDays.Text = "Neutral days: " + NeutralDays;
+            lb_SadDays.Text = "Sad days: " + SadDays;
+            lb_NotSet.Text = "Not set days: " + NotSet;
         }
     }
 }
